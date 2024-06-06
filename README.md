95f07cd5b4e08d358778af46d9a3f030c57cb5ff
duy1009 <duy10092001@gmail.com>
2024-06-06 17:26:27 +0700

    add project

---
+++ README.md
@@ -0,0 +1,18 @@
+## Setup on server
+1. Pull image
+```
+docker pull duy1009/table-recontruct
+```
+
+2. Run:
+```
+cd /path/to/compose.yaml
+sudo docker compose up 
+```
+
+## Using API
+Run:
+```
+python3 test_request.py
+```
+
---
+++ test_request.py
@@ -0,0 +1,32 @@
+import requests
+
+HOST = '127.0.0.1'
+PORT = '8001'
+
+image_path = r"/home/hungdv/tcgroup/Clone_project/table_reconstruction/img_test/B.png"
+
+def htmlTableAPI(img_path):
+    files = {"image": open(img_path, 'rb')}
+    resp = requests.post(f'http://{HOST}:{PORT}/extract-table-v2', files=files)
+    
+    if resp.status_code == 200:
+        data_recv = resp.json()
+        return data_recv
+    else:
+        return None
+
+# Return [bbox, cell_information]
+#       - bbox: [x1, y1, x2, y2]
+#       - cell_information: [index_collum, index_row, span_collum, span_row]
+
+table = htmlTableAPI(image_path)
+
+try:
+    f = open("table.html", "w")
+    f.write(table["table"])
+    f.close()
+
+    print(table["table"])
+except:
+    print(table)
+
